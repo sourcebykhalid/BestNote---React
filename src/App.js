7c4d68764be501a84bb5ac95ffc49432c296609a
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-09-14 15:10:05 +0530

    "Created navbar using routes in App.js"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import { Home } from "./Components/Home";
+import About from "./Components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoteBook app</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
