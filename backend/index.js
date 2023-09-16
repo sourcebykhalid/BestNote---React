d91c2b203eb8ed888d3bdd0a0ba0b42616193737
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-09-16 14:21:36 +0530

    Edit, and delete method added to the iNotebook app to edit or delete the Notes from the client side as well as from the Database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
+var cors = require("cors");
 const connectToMongo = require("./db");
 const express = require("express");
 connectToMongo();
 const app = express();
 const port = 5000;
+
+app.use(cors());
 app.use(express.json());
 //Available Routes
 app.use("/api/auth", require("./routes/auth"));
